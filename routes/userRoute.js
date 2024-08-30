3a8eb840d40f38e3f059f5ab11148bf94eb736d5
12BAIDEN <107093502+12BAIDEN@users.noreply.github.com>
2024-08-30 21:18:01 +0000

    commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -24,8 +24,19 @@ nonAuthRoute.post("/admin/type", UserController.uploadTypes)
 */
 nonAuthRoute.get("/donations/:id", UserController.getDonationType)
 
-app.put('/donations/:title', UserController.updateDonationByTitle);
-app.delete('/donations/:title', UserController.deleteDonationByTitle);
+/*
+* Update a donation by its title.
+* Method: PUT
+* Access: Restricted to authorized administrators.
+*/
+nonAuthRoute.put("/admin/donation/:title", UserController.updateDonation);
+
+/**
+* Delete a donation by its title.
+* Method: DELETE
+* Access: Restricted to authorized administrators.
+*/
+nonAuthRoute.delete("/admin/donation/:title", UserController.deleteDonation);
 
 
 export { nonAuthRoute };
